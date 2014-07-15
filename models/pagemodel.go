345debc6a9a41166900875d1aee15ecac21788ca
skiesel <skiesel@cs.unh.edu>
2014-07-15 11:33:35 -0400

    removed the default beer and tastings notes for new accounts also fixed a typo when building the html elements after adding a new beer

--- models/pagemodel.go
+++ models/pagemodel.go
@@ -35,27 +35,6 @@ func NewPage(r *http.Request) Page {
 
 		if newPage.Account == nil {
 			newPage.Account = NewAccount("NewUser", usr.Email, r)
-			beer := newBeer("Sierra Nevada Pale Ale", "", "2013-07-01", "", 1)
-			beer.ID = 0
-			newPage.Account.CellarsByID[0].Beers[beer.Name] = beer
-			newPage.Account.CellarsByID[0].BeersByID[beer.ID] = beer
-
-			beer = newBeer("Sierra Nevada Bigfoot Barleywine", "", "2014-06-01", "", 10)
-			beer.ID = 1
-			beer.TastingsByID[0] = &Tasting{
-				ID : 0,
-				Rating : 5,
-				Notes: "Sample Note",
-				Date:  Now(),
-			}
-
-			beer.NextTastingID = 1
-
-			newPage.Account.CellarsByID[0].Beers[beer.Name] = beer
-			newPage.Account.CellarsByID[0].BeersByID[beer.ID] = beer
-
-		newPage.Account.CellarsByID[0].NextBeerID = 2
-
 			SaveAccount(c, newPage.Account)
 		}
 	}
